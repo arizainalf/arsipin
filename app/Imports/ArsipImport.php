ed5a804cdbacb322175b3c016d2532b097a8742c
arizainalf <arizainalf@gmail.com>
2024-10-21 17:25:53 +0700

    update

--- app/Imports/ArsipImport.php
+++ app/Imports/ArsipImport.php
@@ -3,13 +3,11 @@
 namespace App\Imports;
 
 use App\Models\Arsip;
-use Ramsey\Uuid\Uuid;
 use App\Models\Riwayat;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToModel;
-use PhpOffice\PhpSpreadsheet\Shared\Date;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 
 class ArsipImport implements ToModel, WithHeadingRow
 {
@@ -19,42 +17,46 @@ class ArsipImport implements ToModel, WithHeadingRow
      */
     public function model(array $row)
     {
-        $kode = $row['deal_type'] . '-' . $row['deal_ref'];
-        $existingArsip = Arsip::where('kode', $kode)->first();
+            dd($row);
+        if ($row['branch'] == '0209') {
+            $kode = $row['deal_type'] . '-' . $row['deal_ref'];
+            $existingArsip = Arsip::where('kode', $kode)->first();
 
-        // Jika kode sudah ada, lewati penambahan data
-        if ($existingArsip) {
+            if ($existingArsip) {
+                return null;
+            }
+            $today = Carbon::today();
+
+            $tanggal_masuk = Date::excelToDateTimeObject($row['contract_date'])->format('Y-m-d');
+            $tanggal_mulai = Date::excelToDateTimeObject($row['start_date'])->format('Y-m-d');
+            $tanggal_selesai = Date::excelToDateTimeObject($row['mat_date'])->format('Y-m-d');
+
+            if ($tanggal_selesai <= $today) {
+                $items = Arsip::whereDate('tanggal_selesai', '<=', $today)->get();
+                $status = '1';
+            } else {
+                $status = '0';
+            }
+
+            $arsip = Arsip::create([
+                'kode' => $kode,
+                'cif' => $row['cif'],
+                'nama_lengkap' => $row['short_name'],
+                'tanggal_mulai' => $tanggal_mulai,
+                'tanggal_selesai' => $tanggal_selesai,
+                'status' => $status,
+            ]);
+
+            $riwayat = Riwayat::create([
+                'arsip_id' => $arsip->id,
+                'jenis' => 'Masuk',
+                'tanggal' => $tanggal_masuk,
+                'catatan' => 'Arsip Masuk',
+            ]);
+
+            return $arsip;
+        } else {
             return null;
         }
-        $today = Carbon::today();
-        
-        $tanggal_masuk = Date::excelToDateTimeObject($row['contract_date'])->format('Y-m-d');
-        $tanggal_mulai = Date::excelToDateTimeObject($row['start_date'])->format('Y-m-d');
-        $tanggal_selesai = Date::excelToDateTimeObject($row['mat_date'])->format('Y-m-d');
-
-        if ($tanggal_selesai <= $today){
-        $items = Arsip::whereDate('tanggal_selesai', '<=', $today)->get();
-            $status = '1';
-        }else{
-            $status = '0';  
-        }
-        
-        $arsip = Arsip::create([
-            'kode' => $kode,
-            'cif' => $row['cif'],
-            'nama_lengkap' => $row['short_name'],
-            'tanggal_mulai' => $tanggal_mulai,
-            'tanggal_selesai' => $tanggal_selesai,
-            'status' => $status,
-        ]);
-
-        $riwayat = Riwayat::create([
-            'arsip_id' => $arsip->id,
-            'jenis' => 'Masuk',
-            'tanggal' => $tanggal_masuk,
-            'catatan' => 'Arsip Masuk',
-        ]);
-
-        return $arsip;
     }
 }
